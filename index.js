107ec2be34bd78141e51522101f385dabadeeeb7
Abdul-Ahad <121967743+ahad324@users.noreply.github.com>
2024-01-17 22:01:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,24 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     window.addEventListener('click', handleWindowClick);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var audioSources = [
+        './part1.mpeg',
+        './part2.mp3',
+        './part3.mp3'
+    ];
+
+    function playSequentialAudio(index) {
+        if (index < audioSources.length) {
+            var audio = new Audio(audioSources[index]);
+            audio.play();
+
+            audio.addEventListener('ended', function () {
+                playSequentialAudio(index + 1);
+            });
+        }
+    }
+
+    playSequentialAudio(0);
+});
