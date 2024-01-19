d641ed1e9b14e7c90d7226ebbdc6ac403b8204c4
Abdul-Ahad <121967743+ahad324@users.noreply.github.com>
2024-01-19 15:06:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -18,27 +18,28 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-        var audioSources = [
-            './part1.mpeg',
-            './part2.mp3',
-            './part3.mp3'
-        ];
-    
-        var currentIndex = 0;
-        function playAudio() {
-            if (currentIndex < audioSources.length) {
-                var audio = new Audio(audioSources[currentIndex]);
-                
-                audio.play().then(() => {
-                    currentIndex++;
-                }).catch((error) => {
-                    console.error("Error playing audio:", error);
-                });
-            }
-        }
+    var audioSources = [
+        './part1.mp3',
+        './part2.mpeg',
+        './part3.mp3',
+        './part4.mp3'
+    ];
 
-        if (window.matchMedia("(max-width: 600px)").matches) {
-            console.log("h")
-            document.body.addEventListener('click', playAudio.bind(this));
+    var currentIndex = 0;
+    function playAudio() {
+        if (currentIndex < audioSources.length) {
+            var audio = new Audio(audioSources[currentIndex]);
+
+            audio.play().then(() => {
+                currentIndex++;
+            }).catch((error) => {
+                console.error("Error playing audio:", error);
+            });
         }
-    });
+    }
+
+    if (window.matchMedia("(max-width: 600px)").matches) {
+        console.log("h")
+        document.body.addEventListener('click', playAudio.bind(this));
+    }
+});
